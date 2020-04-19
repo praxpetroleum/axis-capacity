455c9a701b073b23d7f405fedd9ff9b25ab89d9d
Evans Thereska <Evans.Thereska@prax.com>
2020-04-19 22:49:41 +0100

    DEV-212 Added new table model

--- AxisCapacity/Scripts/SQL/02-new-depot-model.sql
+++ AxisCapacity/Scripts/SQL/02-new-depot-model.sql
@@ -1,48 +1,43 @@
-drop table dbo.TerminalGrouping;
-go
-
 drop table dbo.Capacities;
 go
 
 drop table dbo.DateCapacities;
 go
 
+
 create table dbo.Depot(
-    id tinyint,
-    name varchar(100),
+    id int identity(1,1),
+    name varchar(256) not null,
     constraint PK_Depot primary key(id)
 )
 go
 
 create table dbo.DepotCapacities (
-    depot_id tinyint not null,
+    depot_id int not null,
     shift varchar(2) not null check (shift in ('AM', 'PM')),
     day varchar(3) not null check (day in ('MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN')),
     load integer,
     deliveries decimal(4, 2),
-    shifts tinyint,
+    shifts int,
     capacity decimal(10, 2),
-    constraint PK_DepotCapacities primary key(depot_id, shift, day)
-    constraint FK_DepotCapacities_Depot foreign key(id) references dbo.Depot(id),
+    constraint PK_DepotCapacities primary key(depot_id, shift, day),
+    constraint FK_DepotCapacities_Depot foreign key(depot_id) references dbo.Depot(id),
 )
 go
 
 create table dbo.DepotDateCapacities (
-    depot_id tinyint not null,
+    depot_id int not null,
     shift varchar(2) not null check (shift in ('AM', 'PM')),
     [date] date,
     load integer,
     deliveries decimal(4, 2),
-    shifts tinyint,
+    shifts int,
     capacity decimal(10, 2),
     constraint PK_DepotDateCapacity primary key(depot_id, shift, [date]),
-    constraint FK_DepotCapacities_Depot foreign key(id) references dbo.Depot(id),
+    constraint FK_DepotDateCapacities_Depot foreign key(depot_id) references dbo.Depot(id),
 )
 go
 
-alter table dbo.Terminals add id tinyint;
-alter table dbo.Terminals add depot_id tinyint;
-alter table dbo.Terminals drop constraint PK_Terminals;
-alter table dbo.Terminals add constraint PK_Terminal primary key id;
-alter table dbo.Terminals add constraint FK_Depot_Terminal foreign key depot_id references dbo.Depot(id);
+alter table dbo.Terminals add depot_id int;
+alter table dbo.Terminals add constraint FK_Depot_Terminal foreign key(depot_id) references dbo.Depot(id);
 
